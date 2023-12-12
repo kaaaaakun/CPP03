5aea7cefe8c2863de6f17b856b80ec98d1ecee30
岡崎崇文 <okazakiTakafumi@okazakitakashibun-no-MacBook-Pro.local>
2023-12-12 16:59:25 +0900

    add ClapTrap.cpp

---
+++ ex01/ClapTrap.cpp
@@ -0,0 +1,10 @@
+#include "CrapTrap.hpp"
+
+
+void CrapTrap::attack(const std::string& target) {
+}
+
+void CrapTrap::takeDamage(unsigned int amount)
+{}
+void CrapTrap::beRepaired(unsigned int amount)
+{}
