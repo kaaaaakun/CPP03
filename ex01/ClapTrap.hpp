3595c6e009deb8f4caac26f6235f6e8485c3b9f9
kaaaaakun_ <you@example.com>
2023-12-19 01:10:17 +0900

    add virtual

--- ex01/ClapTrap.hpp
+++ ex01/ClapTrap.hpp
@@ -18,7 +18,7 @@ class ClapTrap {
   ClapTrap(const ClapTrap &other);
   ClapTrap &operator=(const ClapTrap &other);
 
-  void attack(const std::string &target);
+  virtual void attack(const std::string &target);
   void takeDamage(unsigned int amount);
   void beRepaired(unsigned int amount);
 };
