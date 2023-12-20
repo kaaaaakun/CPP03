ead448bf070c2851355fcd3481bc61ef64c9461d
kaaaaakun <>
2023-12-20 19:35:29 +0900

    fix ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,19 +6,23 @@ int main(void) {
   ClapTrap clap3("Clap3");
   ClapTrap clap4("Clap4");
 
+  std::cout << RED <<"------clap1 nomal test----------------------" << END << std::endl;
   clap1.takeDamage(5);
   clap1.takeDamage(5);
   clap1.takeDamage(5);
   clap1.beRepaired(5);
   clap1.attack("Enemy");
 
+  std::cout << RED << "------clap2 attack test----------------------" << END <<std::endl;
   for (int i = 0; i < 12; i++) {
     std::cout << i << " : ";
     clap2.attack("Enemy");
   }
 
+  std::cout << RED << "------clap3 damage test----------------------" << END << std::endl;
   clap3.takeDamage(100);
 
+  std::cout << RED << "------clap4 repaired test----------------------" << END << std::endl;
   clap4.takeDamage(3);
   clap4.beRepaired(100);
   clap4.attack("Enemy");
---
+++ ex02/obj/main.d
@@ -0,0 +1,7 @@
+obj/main.o: main.cpp ClapTrap.hpp FragTrap.hpp ScavTrap.hpp
+
+ClapTrap.hpp:
+
+FragTrap.hpp:
+
+ScavTrap.hpp:
